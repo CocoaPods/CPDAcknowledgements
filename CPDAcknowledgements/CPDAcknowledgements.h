6017925af0316c5a03cc4d4c1f18ed69b9654105
ladland <ladland@somewhereonline.info>
2016-06-26 17:44:53 +1000

    Create CPDAcknowledgements.h
    Created Umbrella Header to allow for Swift ModuleMap Creation

---
+++ CPDAcknowledgements/CPDAcknowledgements.h
@@ -0,0 +1,16 @@
+//
+//  CPDAcknowledgements
+//  Umbrella Header to allow for Swift ModuleMap Creation
+//
+//  Created by Lindsay Adland on 18/6/16.
+//  Copyright © 2016 ClosedCaption. All rights reserved.
+
+#ifndef CPDAcknowledgements_h
+#define CPDAcknowledgements_h
+
+#import "CPDAcknowledgementsViewController.h"
+#import "CPDContribution.h"
+#import "CPDLibrary.h"
+#import "CPDStyle.h"
+
+#endif
